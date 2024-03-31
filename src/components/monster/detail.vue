0bd33a7afddfd3799b9ff643f2a325c15e44ab95
zhanghao <archer03371@163.com>
2024-03-31 23:15:37 +0800

    change monster detail

--- src/components/monster/detail.vue
+++ src/components/monster/detail.vue
@@ -3,39 +3,9 @@
     <comp-icon-monster name="MonsterDetail.IconMonster"
       :monster="monster" />
 
-    <!-- 
-    <div class="monster-icon">
-      <div class="icon monster" 
-        :data-cardid="monster.monsterId" 
-        :data-cards-pic-idx="Math.ceil(monster.monsterId/100)"
-        :data-cards-pic-x="(monster.monsterId-1)%10"
-        :data-cards-pic-y="Math.floor((monster.monsterId-1)%100/10)"
-      >
-        <div class="attrs">
-          <div class="attr" :data-attr="monster.attr" />
-          <div class="attr" :data-attr="monster.subAttr" />
-          <div class="attr" :data-attr="monster.thirdAttr" />
-        </div>
-        <div class="id">{{ monster.monsterId }}</div>
-      </div>
-    </div> 
-    -->
     <div class="monster-basic-info">
       <div class="monster-name">{{ monster.name }}</div>
-      <div class="monster-info">
-        <span v-if="monster.typeId!=-1"
-          class="type type-icon"
-          :data-type-icon="monster.typeId"
-        />
-        <span v-if="monster.subTypeId!=-1"
-          class="type type-icon"
-          :data-type-icon="monster.subTypeId"
-        />
-        <span v-if="monster.extraTypeId!=-1"
-          class="type type-icon"
-          :data-type-icon="monster.extraTypeId"
-        />
-        
+      <div class="monster-info">        
         <div class="hp">
           <div class="tag">HP:</div>
           <div class="value">{{ monster.maxHP }}</div>
@@ -52,26 +22,46 @@
       </div>
     </div>
 
-    <!--覚醒-->
-    <div class="askill-info">
-      <comp-icon-awaken v-for="(awakenskillId, index) in monster.awakenskillIds"
-        name="MonsterDetail.awakenskill"
-        :awakenskillId="awakenskillId"
-        :isSearchMode="false"
-        :key="monster.monsterId + '.awakenskill.' + index"
+    <div class="monster-icon-info">
+      <div class="monster-icon-type">
+        <span v-if="monster.typeId!=-1"
+          class="type type-icon"
+          :data-type-icon="monster.typeId"
         />
+        <span v-if="monster.subTypeId!=-1"
+          class="type type-icon"
+          :data-type-icon="monster.subTypeId"
+        />
+        <span v-if="monster.extraTypeId!=-1"
+          class="type type-icon"
+          :data-type-icon="monster.extraTypeId"
+        />
+      </div>
+
+      <!--覚醒-->
+      <div class="askill-info">
+        <comp-icon-awaken v-for="(awakenskillId, index) in monster.awakenskillIds"
+          name="MonsterDetail.awakenskill"
+          :awakenskillId="awakenskillId"
+          :isSearchMode="false"
+          :key="monster.monsterId + '.awakenskill.' + index"
+          />
+      </div>
+
+      <!--超覚醒-->
+      <div class="super-askill-info">
+        <comp-icon-awaken v-for="(superawakenskillId, index) in monster.superawakenskillIds"
+          name="MonsterDetail.superawakenskillId"
+          :awakenskillId="superawakenskillId"
+          :isSearchMode="false"
+          :key="monster.monsterId + '.superawakenskill.' + index"
+          />
+      </div>
     </div>
 
-    <!--超覚醒-->
-    <div class="super-askill-info">
-      <comp-icon-awaken v-for="(superawakenskillId, index) in monster.superawakenskillIds"
-        name="MonsterDetail.superawakenskillId"
-        :awakenskillId="superawakenskillId"
-        :isSearchMode="false"
-        :key="monster.monsterId + '.superawakenskill.' + index"
-        />
-    </div>
-    <div class="skill">
+
+    <!--スキル-->
+    <div class="skill" v-if="false">
       <div class="name" @click="findBySkill(monster)">{{monster.skill != undefined ?  monster.skill.name : ''}}</div>
       <div class="turn" v-if="monster.skill != undefined && monster.skill.turn != undefined">
         {{monster.skill != undefined ?  monster.skill.turn : ''}}
@@ -80,7 +70,8 @@
         {{monster.skill != undefined ?  monster.skill.gameDesc : ''}}
       </div>
     </div>
-    <div class="leaderskill">
+    <!--リーダースキル-->
+    <div class="leaderskill" v-if="false">
       <div class="name">{{monster.leaderskill != undefined ?  monster.leaderskill.name : ''}}</div>
       <div class="desc">{{monster.leaderskill != undefined ?  monster.leaderskill.gameDesc : ''}}</div>
     </div>
